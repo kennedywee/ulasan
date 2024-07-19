3d9c0b31500e2ae74d4d6a4a16bcbbe41db5ccf2
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-07-19 20:28:50 +0800

    update user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create!(user_params)
+    start_new_session_for @user
+    redirect_to root_url
+  rescue ActiveRecord::RecordNotUnique
+    redirect_to new_session_url(email_address: user_params[:email_address])
   end
 
   private
