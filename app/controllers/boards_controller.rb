29a7890ba2436490d14ec984861795e84185613b
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-07-19 23:33:32 +0800

    update boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,12 @@
 class BoardsController < ApplicationController
+  before_action :set_user, only: %i[ new create ]
+  before_action :set_board, only: %i[ show ]
+
   def index
     @boards = Board.all
+  end
+
+  def show
   end
 
   def new
@@ -8,11 +14,26 @@ class BoardsController < ApplicationController
   end
 
   def create
-    @board = Board.create!(board_params)
+    @board = Board.new(board_params)
+    @board.user = @user
+
+    if @board.save
+      redirect_to @board, notice: "Board was succesfully created."
+    else
+     render :new
+    end
   end
 
   private
     def board_params
-      params.require(:board).permit(:user_id, :appname, :title, :description, :website)
+      params.require(:board).permit(:appname, :title, :description, :website)
+    end
+
+    def set_user
+      @user = Current.user
+    end
+
+    def set_board
+      @board = Board.find(params[:id])
     end
 end
