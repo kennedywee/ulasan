dd6f63a16aa82b6c63e89b69a24d382fc417d824
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-07-19 20:13:19 +0800

    add user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,16 @@ class UsersController < ApplicationController
   allow_unauthenticated_access
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create!(user_params)
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :username, :email_address, :password)
+    end
 end
