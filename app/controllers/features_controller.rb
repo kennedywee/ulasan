b3b462b165d8225c30e91b914058a67905ed3446
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-07-19 22:39:51 +0800

    update feature controller

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -2,11 +2,19 @@ class FeaturesController < ApplicationController
   allow_unauthenticated_access only: %i[index]
 
   def index
+    @features = Feature.all
   end
 
   def new
+    @feature = Feature.new
   end
 
   def create
+    @feature = Feature.create!(feature_params)
   end
+
+  private
+    def feature_params
+      params.require(:feature).permit(:user_id, :board_id, :title, :description, :category)
+    end
 end
