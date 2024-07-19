f56eedf483aac859fd6b19a2e4c7cd40b201e470
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-07-19 22:47:31 +0800

    board stuff

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,17 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.create!(board_params)
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:user_id, :appname, :title, :description, :website)
+    end
 end
