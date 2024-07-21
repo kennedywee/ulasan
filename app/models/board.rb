49ea63591bf1937c1dd71dbd12e13f2f14a595fd
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-07-21 22:30:06 +0800

    update board form error

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,7 @@ class Board < ApplicationRecord
   belongs_to :user
   has_many :features
 
+  validates :appname, presence: true
   validates :title, presence: true
   validates :description, presence: true
   validates :website, presence: true
