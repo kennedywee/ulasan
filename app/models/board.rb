a6ba0a4ac8f1943c2356aad9d50b783d36c49d51
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-07-24 18:38:15 +0800

    update board card

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,8 +2,6 @@ class Board < ApplicationRecord
   belongs_to :user
   has_many :features
 
-  enum :label, %w[open interesting in-development shipped closed], default: :open
-
   validates :appname, presence: true
   validates :description, presence: true
   validates :website, presence: true
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,5 +2,7 @@ class Feature < ApplicationRecord
   belongs_to :user
   belongs_to :board
 
+  enum :label, %w[open interesting in-development shipped closed], default: :open
+
   validates :title, presence: true
 end
