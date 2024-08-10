28f2245d2b03c8b0ab8be7cceed7f8cc2982dc5b
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-08-11 00:29:09 +0800

    add format validation for appname

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,7 +2,16 @@ class Board < ApplicationRecord
   belongs_to :user
   has_many :features
 
-  validates :appname, presence: true
+  # before_validation :format_appname
+
+  validates :appname, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validates :description, presence: true
   validates :website, presence: true
+
+  private
+
+  def format_appname
+    # Convert to lowercase, replace spaces with hyphens, and remove special characters
+    self.appname = appname.to_s.downcase.gsub(/\s+/, "-").gsub(/[^a-z0-9\-]/, "")
+  end
 end
